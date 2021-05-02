6893d23fc57ff948e370f20bf1dc3a00d841c355
Rus <valuiskiuruslan@gmail.com>
2021-05-02 15:23:47 +0300

    Added reviews styles and scripts

--- coding-exercise-with-reviews.php
+++ coding-exercise-with-reviews.php
@@ -17,6 +17,7 @@ defined('ABSPATH') || exit;
 define('CEWR_VERSION', '1.1');
 define('CEWR_PLUGIN', __FILE__);
 define('CEWR_PLUGIN_DIR', untrailingslashit(dirname(__FILE__)));
+define('CEWR_REVIEWS_PAGE_TEMPLATE', 'reviews.php');
 
 require_once CEWR_PLUGIN_DIR . '/classes/Template.php';
 
@@ -24,7 +25,7 @@ require_once CEWR_PLUGIN_DIR . '/classes/Template.php';
 add_filter('page_template', 'cewr_page_template');
 function cewr_page_template($page_template)
 {
-    if (get_page_template_slug() == 'reviews.php') {
+    if (get_page_template_slug() == CEWR_REVIEWS_PAGE_TEMPLATE) {
         $page_template = Template::get_template('reviews.php', '', '', '', true);
     }
     return $page_template;
@@ -40,4 +41,34 @@ function cewr_add_template_to_select($post_templates, $wp_theme, $post, $post_ty
     $post_templates['reviews.php'] = __('Reviews', 'coding-exercise-with-reviews');
 
     return $post_templates;
+}
+
+add_action('wp_enqueue_scripts', 'cewr_do_enqueue_scripts', 10, 0);
+function cewr_do_enqueue_scripts()
+{
+    if (get_page_template_slug() == CEWR_REVIEWS_PAGE_TEMPLATE) {
+        wp_enqueue_style('cewr-style', cewr_plugin_url('assets/css/style.css'), array(), CEWR_VERSION);
+
+        wp_enqueue_script('cewr-jquery-star-rating-svg', cewr_plugin_url('assets/js/jquery.star-rating-svg.min.js'),
+            array('jquery'), CEWR_VERSION, true);
+        wp_enqueue_script('cewr-script', cewr_plugin_url('assets/js/script.js'),
+            array('cewr-jquery-star-rating-svg'), CEWR_VERSION, true);
+    }
+}
+
+/**
+ * Returns plugin url for given path
+ * @param string $path
+ * @return string
+ */
+function cewr_plugin_url($path = '')
+{
+    $url = plugins_url($path, CEWR_PLUGIN);
+
+    if (is_ssl()
+        and 'http:' == substr($url, 0, 5)) {
+        $url = 'https:' . substr($url, 5);
+    }
+
+    return $url;
 }
--- templates/reviews.php
+++ templates/reviews.php
@@ -4,12 +4,97 @@
  * @version 1.0.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
 ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="reviews-header">
+            <h1><?php the_title() ?></h1>
+        </header>
 
-<h1><?php the_title() ?></h1>
+        <div class="review-table-wrap">
+            <table class="review-table">
+                <thead>
+                <tr>
+                    <th>Casino</th>
+                    <th>Bonus</th>
+                    <th>Features</th>
+                    <th>Play</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td data-label="Casino">
+                        <div>
+                            <div class="review-logo">
+                                <img src="https://picsum.photos/195/75" alt="">
+                            </div>
+                            <a href="#">Review</a>
+                        </div>
+                    </td>
+                    <td data-label="Bonus">
+                        <div>
+                            <div class="svg-star-rating" data-rating="4"></div>
+                            <span class="bonus-description">Free $25 bonus and 100% deposit up to $1000</span>
+                        </div>
+                    </td>
+                    <td data-label="Features">
+                        <ul class="review-features">
+                            <li>Easy cash back options</li>
+                            <li>Good payment options</li>
+                            <li>Exclusive game</li>
+                        </ul>
+                    </td>
+                    <td data-label="Play">
+                        <div>
+                            <a href="#" class="review-btn">Play now</a>
+                            <div class="review-terms-and-conditions">
+                                21+ | <a href="https://generator.lorem-ipsum.info/terms-and-conditions">T&CS Apply</a> |
+                                Gamble
+                                Responsibly
+                            </div>
+                        </div>
+                    </td>
+                </tr>
 
+                <tr>
+                    <td data-label="Casino">
+                        <div>
+                            <div class="review-logo">
+                                <img src="https://picsum.photos/195/75" alt="">
+                            </div>
+                            <a href="#">Review</a>
+                        </div>
+                    </td>
+                    <td data-label="Bonus">
+                        <div>
+                            <div class="svg-star-rating" data-rating="4"></div>
+                            <span class="bonus-description">Free $25 bonus and 100% deposit up to $1000</span>
+                        </div>
+                    </td>
+                    <td data-label="Features">
+                        <ul class="review-features">
+                            <li>Easy cash back options</li>
+                            <li>Good payment options</li>
+                            <li>Exclusive game</li>
+                        </ul>
+                    </td>
+                    <td data-label="Play">
+                        <div>
+                            <a href="#" class="review-btn">Play now</a>
+                            <div class="review-terms-and-conditions">
+                                21+ | <a href="https://generator.lorem-ipsum.info/terms-and-conditions">T&CS Apply</a> |
+                                Gamble
+                                Responsibly
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </article>
 <?php
 get_footer();

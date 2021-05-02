b5983ad3229d8462c983b97e0308ee6bd4781caf
Rus <valuiskiuruslan@gmail.com>
2021-05-02 14:11:26 +0300

    Added reviews page template

---
+++ templates/reviews.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package CodingExerciseWithReviews/Templates
+ * @version 1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+
+<h1><?php the_title() ?></h1>
+
+<?php
+get_footer();

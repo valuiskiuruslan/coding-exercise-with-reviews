b74f621f4bd035746c5e45aeae012e2bf5ae6d12
Rus <valuiskiuruslan@gmail.com>
2021-05-03 19:05:38 +0300

    Displayed reviews into page template

--- templates/reviews.php
+++ templates/reviews.php
@@ -6,6 +6,8 @@
 
 defined('ABSPATH') || exit;
 
+use CodingExerciseWithReviews\classes\Review;
+
 get_header();
 ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -17,81 +19,79 @@ get_header();
             <table class="review-table">
                 <thead>
                 <tr>
-                    <th>Casino</th>
-                    <th>Bonus</th>
-                    <th>Features</th>
-                    <th>Play</th>
+                    <th><?= __('Casino', 'coding-exercise-with-reviews') ?></th>
+                    <th><?= __('Bonus', 'coding-exercise-with-reviews') ?></th>
+                    <th><?= __('Features', 'coding-exercise-with-reviews') ?></th>
+                    <th><?= __('Play', 'coding-exercise-with-reviews') ?></th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td data-label="Casino">
-                        <div>
-                            <div class="review-logo">
-                                <img src="https://picsum.photos/195/75" alt="">
-                            </div>
-                            <a href="#">Review</a>
-                        </div>
-                    </td>
-                    <td data-label="Bonus">
-                        <div>
-                            <div class="svg-star-rating" data-rating="4"></div>
-                            <span class="bonus-description">Free $25 bonus and 100% deposit up to $1000</span>
-                        </div>
-                    </td>
-                    <td data-label="Features">
-                        <ul class="review-features">
-                            <li>Easy cash back options</li>
-                            <li>Good payment options</li>
-                            <li>Exclusive game</li>
-                        </ul>
-                    </td>
-                    <td data-label="Play">
-                        <div>
-                            <a href="#" class="review-btn">Play now</a>
-                            <div class="review-terms-and-conditions">
-                                21+ | <a href="https://generator.lorem-ipsum.info/terms-and-conditions">T&CS Apply</a> |
-                                Gamble
-                                Responsibly
-                            </div>
-                        </div>
-                    </td>
-                </tr>
+                <?php
+                $reviews = Review::find([
+                    'post_status' => 'publish',
+                    'orderby' => 'menu_order',
+                ]);
 
-                <tr>
-                    <td data-label="Casino">
-                        <div>
-                            <div class="review-logo">
-                                <img src="https://picsum.photos/195/75" alt="">
-                            </div>
-                            <a href="#">Review</a>
-                        </div>
-                    </td>
-                    <td data-label="Bonus">
-                        <div>
-                            <div class="svg-star-rating" data-rating="4"></div>
-                            <span class="bonus-description">Free $25 bonus and 100% deposit up to $1000</span>
-                        </div>
-                    </td>
-                    <td data-label="Features">
-                        <ul class="review-features">
-                            <li>Easy cash back options</li>
-                            <li>Good payment options</li>
-                            <li>Exclusive game</li>
-                        </ul>
-                    </td>
-                    <td data-label="Play">
-                        <div>
-                            <a href="#" class="review-btn">Play now</a>
-                            <div class="review-terms-and-conditions">
-                                21+ | <a href="https://generator.lorem-ipsum.info/terms-and-conditions">T&CS Apply</a> |
-                                Gamble
-                                Responsibly
-                            </div>
-                        </div>
-                    </td>
-                </tr>
+                if (!empty($reviews) && is_array($reviews)) {
+                    foreach ($reviews as $review) {
+                        /** @var Review $review */
 
+                        $properties = $review->get_properties();
+                        $rating = !empty($properties[Review::META_RATING]) ? $properties[Review::META_RATING] : 0;
+                        $bonus = !empty($properties[Review::META_BONUS]) ? $properties[Review::META_BONUS] : '';
+                        $play_url = !empty($properties[Review::META_PLAY_URL]) ? $properties[Review::META_PLAY_URL] : '#';
+                        $terms_and_conditions = !empty($properties[Review::META_TERMS_AND_CONDITIONS])
+                            ? $properties[Review::META_TERMS_AND_CONDITIONS]
+                            : '';
+
+                        $features = !empty($properties[Review::META_FEATURES])
+                            ? preg_split('/\r\n|\r|\n/', $properties[Review::META_FEATURES])
+                            : null;
+                        ?>
+                        <tr>
+                            <td data-label="<?= __('Casino', 'coding-exercise-with-reviews') ?>">
+                                <div>
+                                    <div class="review-logo">
+                                        <?= get_the_post_thumbnail($review->get_id()) ?>
+                                    </div>
+                                    <a href="<?= get_permalink($review->get_id()) ?>">
+                                        <?= __('Review', 'coding-exercise-with-reviews') ?>
+                                    </a>
+                                </div>
+                            </td>
+                            <td data-label="<?= __('Bonus', 'coding-exercise-with-reviews') ?>">
+                                <div>
+                                    <div class="svg-star-rating" data-rating="<?= $rating ?>"></div>
+                                    <span class="bonus-description"><?= $bonus ?></span>
+                                </div>
+                            </td>
+                            <td data-label="<?= __('Features', 'coding-exercise-with-reviews') ?>">
+                                <?php
+                                if (!empty($features) && is_array($features)) { ?>
+                                    <ul class="review-features">
+                                        <?php
+                                        foreach ($features as $feature) { ?>
+                                            <li><?= $feature ?></li>
+                                            <?php
+                                        } ?>
+                                    </ul>
+                                    <?php
+                                } ?>
+                            </td>
+                            <td data-label="<?= __('Play', 'coding-exercise-with-reviews') ?>">
+                                <div>
+                                    <a href="<?= $play_url ?>" class="review-btn">
+                                        <?= __('Play now', 'coding-exercise-with-reviews') ?>
+                                    </a>
+                                    <div class="review-terms-and-conditions">
+                                        <?= $terms_and_conditions ?>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                } ?>
                 </tbody>
             </table>
         </div>
